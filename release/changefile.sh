6b6887f4e6823bddcbb510bc2a58d3f7ebfd9821
jb-kerboeuf <jean-baptiste.kerboeuf@johnpaul.com>
2017-10-27 11:39:12 +0200

    test

---
+++ release/changefile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Modify travis build wich block deploy
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_NAME}"
+git config --global push.default simple
+msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
+echo "$msg" >> $TRAVIS_BUILD_DIR/build.txt
+git add $TRAVIS_BUILD_DIR/build.txt
+git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
